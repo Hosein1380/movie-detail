95df90349afeb50d025c64837d56ae1b189ab1d0
Hosein1380 <ghafary.tm37@gmail.com>
2022-08-31 17:44:14 +0430

    feat - > implemt login route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,17 @@ import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { appRoutes } from './app.routing';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     RouterModule,
-    // RouterModule.forRoot(),
+    RouterModule.forRoot(appRoutes),
     HttpClientModule
   ],
   providers: [],
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,6 +2,10 @@ import { Routes } from '@angular/router';
 
 export const appRoutes: Routes = [
 
-
+  {
+    path: '',
+    loadChildren: () => import("./login/login.module").then((module) => module.LoginModule),
+  },
 
 ]
+
