432d9d388cee54f252f79c16a8a3cd60b2619d90
Hosein1380 <ghafary.tm37@gmail.com>
2022-09-01 14:39:46 +0430

    feat - > implement login form validators

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { LoginComponent } from './login.component';
 import { loginRoutes } from './login.routing';
@@ -13,6 +14,7 @@ import { loginRoutes } from './login.routing';
   imports: [
       CommonModule,
       RouterModule.forChild(loginRoutes),
+      ReactiveFormsModule
   ],
   providers: [],
 })
